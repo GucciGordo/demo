2ec85d59fd7b08f9007ee27d6a0564385653325f
GucciG <42783274+GucciGordo@users.noreply.github.com>
2022-08-21 17:14:35 -0800

    function in js for var scoping

--- js/add-content.js
+++ js/add-content.js
@@ -1,19 +1,22 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
-
-if(hourNow > 18) {
-    greeting = 'Good evening ';
-} else if(hourNow > 12) {
-    greeting = 'Good afternoon ';
-} else if(hourNow > 0) {
-    greeting = 'Good morning ';
-} else {
-    greeting = 'Welcome ';
+function Greet(){
+    var today = new Date();
+    var hourNow = today.getHours();
+    var greeting;
+    
+    if(hourNow > 18) {
+        greeting = 'Good evening ';
+    } else if(hourNow > 12) {
+        greeting = 'Good afternoon ';
+    } else if(hourNow > 0) {
+        greeting = 'Good morning ';
+    } else {
+        greeting = 'Welcome ';
+    }
+    
+    var elWelcome = document.getElementById('welcome');
+    elWelcome.innerHTML = greeting;
 }
-
-var elWelcome = document.getElementById('welcome');
-elWelcome.innerHTML = greeting;
+Greet();
 
 
 
